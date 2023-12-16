e0ba4f144a667f5d2db398897225a6a4935b4351
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-16 21:18:03 +0300

    Copmleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// index.js
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'customer';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new customer'; // This will cause an error because you can't reassign a constant.
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  leastFavoriteCustomer,
+  changeLeastFavoriteCustomer,
+};
+
